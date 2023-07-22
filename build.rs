b7ba5ac1cf22779d52e321d13195024e50853228
kimjongbing <126769537+kimjongbing@users.noreply.github.com>
2023-07-22 14:28:00 +1000

    file paths are relative now

--- build.rs
+++ build.rs
@@ -1,9 +1,8 @@
 fn main() {
-
   cxx_build::bridge("src/lib.rs")
-    .file("F:/resources/codingstuff/dll_injector_uwp/cpp/bridge.cpp")
-    .include("F:/resources/codingstuff/dll_injector_uwp/cpp")
-    .compile("dll_injector_uwp");
+      .file("cpp/bridge.cpp")
+      .include(".")
+      .compile("dll_injector_uwp");
 
   println!("cargo:rerun-if-changed=src/lib.rs");
   println!("cargo:rerun-if-changed=cpp/bridge.cpp");
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
 #[cxx::bridge]
 pub mod dll_injector {
-  unsafe extern "C++" {
-    include!("F:/resources/codingstuff/dll_injector_uwp/cpp/bridge.h"); 
+    unsafe extern "C++" {
+        include!("cpp/bridge.h");
 
-    fn launch_process(exe_path: String) -> i32;
-    fn load_dll(dll_path: String) -> i32;
-    fn enumerate_processes() -> i32;
-    fn inject_dll(pid: i32, dll_path: String) -> i32;
-  }
+        fn launch_process(exe_path: String) -> i32;
+        fn load_dll(dll_path: String) -> i32;
+        fn enumerate_processes() -> i32;
+        fn inject_dll(pid: i32, dll_path: String) -> i32;
+    }
 }
-
